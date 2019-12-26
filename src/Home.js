dd070e2884e18d72de1eda69c430c3b071378a42
Michelle <wong.mich.c@gmail.com>
2019-12-26 13:49:39 +1100

    image

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,7 @@ function Home ()
   return (<div>
      <div><Navbar /></div>
      <div className={styles.responsive}>
-     <img src='./todoImage.jpg' className='responsive' alt='todo-list' width='1000' height='800'/>
+     <img src='./todoImage.jpg' className={styles.responsive} alt='todo-list' width='1000' height='800'/>
   
     </div>
   </div>)
